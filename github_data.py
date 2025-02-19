52eb7b53f54125e4c9b2737f55504d19f685700b
pedrosteinmuller <pedrosteinmuller10105@hotmail.com>
2025-02-19 11:05:49 -0300

    feat: created function to get repositories by year

---
+++ github_data.py
@@ -0,0 +1,35 @@
+import requests
+import pandas as pd
+import time
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+HEADERS = {"Authorization": f"token {GITHUB_TOKEN}"}
+
+def get_repositories_by_year(year, per_page=100, pages=10):
+    repositories = []
+    url = "https://api.github.com/search/repositories"
+
+    for page in range(1, pages + 1):
+        print(f"Coletando repositórios do ano {year}, página {page}...")
+        params = {
+            "q": f"created:{year}-01-01..{year}-12-31",
+            "per_page": per_page,
+            "page": page,
+            "sort": "stars",
+            "order": "desc"
+        }
+        response = requests.get(url, headers=HEADERS, params=params)
+
+        if response.status_code == 200:
+            data = response.json()
+            repositories.extend(data["items"])
+        else:
+            print(f"Erro ao coletar dados: {response.status_code}")
+            break
+
+        time.sleep(5)
+
+    return repositories
